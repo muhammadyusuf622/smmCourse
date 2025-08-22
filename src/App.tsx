3d3d5713f2b38d9cc591a48984edc95792f3ca71
Muhammad Yusuf <yuvsufn@gmail.com>
2025-08-22 18:03:59 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import derectorImg from "./assets/images/rahmar.jpg";
 import CourseRec from "./components/CourseRec";
 import LessonCars from "./components/LessonCars";
